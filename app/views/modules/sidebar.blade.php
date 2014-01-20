85b66d85a82f55ede3cb9e48318a7db9e1e0bdb8
Reece Sellars <rsellars355@gmail.com>
2014-01-20 00:28:57 +0000

    Updated sidebar URLs

--- app/views/modules/sidebar.blade.php
+++ app/views/modules/sidebar.blade.php
@@ -1,6 +1,5 @@
 <div class="sidebar">
-    <div class="sidebar__portrait"></div>
-    <nav>
+    <nav class="sidebar__nav">
         <ul>
             <li>
                 <a href="/">
@@ -14,34 +13,15 @@
                     <span>Work</span>
                 </a>
             </li>
-            <li>
-                <a href="/plans">
-                    <i class="sidebar__icon sidebar__icon--plans">P</i>
-                    <span>Plans</span>
-                </a>
-            </li>
-            <li>
                 <a href="/contact">
                     <i class="sidebar__icon sidebar__icon--contact">C</i>
                     <span>Contact</span>
                 </a>
             </li>
             <li>
-                <a href="/employ">
-                    <i class="sidebar__icon sidebar__icon--Employ">E</i>
+                <a href="https://github.com/reecss/CV" target="_blank">
+                    <i class="sidebar__icon sidebar__icon--employ">E</i>
                     <span>Employ</span>
-                </a>
-            </li>
-            <li>
-                <a href="/blog">
-                    <i class="sidebar__icon sidebar__icon--Blog">B</i>
-                    <span>Blog</span>
-                </a>
-            </li>
-            <li>
-                <a href="/playground">
-                    <i class="sidebar__icon sidebar__icon--playground">P</i>
-                    <span>Playground</span>
                 </a>
             </li>
         </ul>
@@ -49,8 +29,10 @@
     <div class="sidebar__toggle"></div>
 
     <ul class="social-links">
-        <li><a href="https://github.com/rsellars1" target="_blank">GitHub</a></li>
+        <li><a href="https://github.com/reecss" target="_blank">GitHub</a></li>
         <li><a href="http://codepen.io/rsellars1" target="_blank">CodePen</a></li>
-        <li><a href="https://twitter.com/rsellars1" target="_blank">Twitter</a></li>
+        <li><a href="https://twitter.com/reecss" target="_blank">Twitter</a></li>
     </ul>
 </div>
+
+<div class="sidebar-mask"></div>
